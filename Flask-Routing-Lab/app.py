4717e6267b2105e25987a21a40e4a32b9e783c5b
eranho23-meet <109900208+eranho23-meet@users.noreply.github.com>
2022-07-27 01:00:21 +0300

    added many stuff
    I have added css, and gave it a background carusele (i don't get why they asked us for this)
    added carts, and some other stuff (basically all of the mandatory stuff)

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -9,29 +9,35 @@ app = Flask(  # Create a flask app
 
 # Your code should be below
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
+@app.route('/shopcart')
+def cart():
+    return render_template('cart.html', carted=[])
+
+
 @app.route('/product-egg')
 def egg():
-    return render_template('product.html', object='egg')
+    return render_template('product.html', object='egg', price='2')
 
 
 @app.route('/product-carrot')
 def carrot():
-    return render_template('product.html', object='carrot')
+    return render_template('product.html', object='carrot', price='1')
 
 
 @app.route('/product-ALMwater')
 def AlmondWater():
-    return render_template('product.html', object='almond water')
+    return render_template('product.html', object='almond water', price='10')
 
 
 @app.route('/product-birdcage')
 def birdcage():
-    return render_template('product.html', object='birdcage')
+    return render_template('product.html', object='birdcage', price='5')
 
 
 # Your code should be above
